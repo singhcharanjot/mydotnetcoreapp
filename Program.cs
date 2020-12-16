1452a53383d6e0e3b91f283f027b4095712da64d
Charanjot Singh <junkproof94@gmail.com>
2020-12-16 17:25:38 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    //webBuilder.UseStartup<Startup>();
+		   // webBuilder.UseUrls("http://localhost:5000", "http://odin:5000", "http://192.168.1.2:5000");
+                    webBuilder.UseStartup<StartupJavaScript>();
+                });
+    }
+}
